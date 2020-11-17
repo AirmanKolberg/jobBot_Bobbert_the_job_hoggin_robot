dc30f3bac9c02063299e5528c255f11da2a593b2
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-11-17 14:24:20 -0700

    Create personal_info.py

---
+++ personal_info.py
@@ -0,0 +1,6 @@
+# Examples of things to keep in here:
+#
+# search_list = ['mcdonalds', 'wendys', 'customer service', 'restaurant']
+# search_list_david = ['python', 'object-oriented programming', 'senior developer', 'AS2']
+# location = 'United States'
+# location_b = 'Springfield, Ohio'
